ebc80b35256c3468b3dc82de8a9e66fc4248f058
lucaspinarj <lucaspinarj@hotmail.com>
2022-07-22 13:24:13 -0300

    updated

--- src/br/com/lucas/pass/Password.java
+++ src/br/com/lucas/pass/Password.java
@@ -1,26 +1,52 @@
 package br.com.lucas.pass;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
-
 public class Password {
 
-	public static void main(String[] args) {
+	static void question() {
 		Creation pw = new Creation();
-		
-		Scanner enter = new Scanner(JOptionPane.showInputDialog("How many characters? (number values)"));
-		int len = enter.nextInt();
-		
-		pw.generatePassword(len);
-		
+		Scanner enter;
+		int len = 0;
+
+		try {
+			do {
+				enter = new Scanner(JOptionPane.showInputDialog(null, "How many characters? (number values)",
+						"Password Generator", JOptionPane.QUESTION_MESSAGE));
+				len = enter.nextInt();
+
+				if (len <= 4) {
+					JOptionPane.showMessageDialog(null, "Don't you want to try a longer password?", "oops!",
+							JOptionPane.PLAIN_MESSAGE);
+				}
+
+				enter.close();
+			} while (len <= 4);
+		} catch (NullPointerException e) {
+			JOptionPane.getRootFrame().dispose();
+		}
+
 		String password = "Your password is: " + pw.generatePassword(len);
-		
 		JOptionPane.showMessageDialog(null, password);
-		
-		
-		enter.close();
+
+		System.out.println(password);
+
+	}
+
+	public static void main(String[] args) {
+
+		boolean sucess = false;
+		while (sucess == false) {
+			try {
+				question();
+				sucess = true;
+			} catch (NoSuchElementException e) {
+				JOptionPane.showMessageDialog(null, "Not a valid input.");
+			}
+		}
 
 	}
 
